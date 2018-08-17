a86c3fde9ea2dd8ddb85bd4cef1731f5189151a0
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2018-08-17 19:53:21 +0100

    total by quarter

--- src/statistics.js
+++ src/statistics.js
@@ -1,3 +1,13 @@
 export function latest_yearquarter(dt) {
     return dt.sql('select yearquarter from df order by yearquarter desc limit 1')[0]['yearquarter']
 }
+
+
+export function total_by_quarter(dt) {
+    return dt.sql(`select yearquarter, yearquarter_end_date, yearquarter_mid_date,
+    sum(receipts) as sum_receipts,
+    sum(disposals) as sum_disposals,
+    sum(outstanding) as sum_outstanding
+    from df
+    group by yearquarter, yearquarter_end_date, yearquarter_mid_date`)
+}
