e81f2e124b5c19d236bdb06f3db4a552a51d036b
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2018-08-18 10:22:45 +0100

    fix sparkline

--- src/sparkline.js
+++ src/sparkline.js
@@ -1,57 +1,73 @@
 import d3 from "d3"
 import _ from "lodash";
 
-export function sparkline(values, time_index, DOM, width = 64, height = 17) {
+export function sparkline(time_series, value_col, date_col, DOM, width = 64, height = 17) {
+  let values = time_series.get_column(value_col)
+  let index = time_series.index
+  let index_col = time_series.index_column
 
-    const x = d3.scaleLinear().domain([0, values.length - 1]).range([0.5, width - 1.5]);
-    const y = d3.scaleLinear().domain([0, _.maxBy(values, d => d[1])[1]]).range([height - 0.5, 0.5]);
-    const context = DOM.context2d(width, height);
-    const line = d3.line().x((d, i) => x(i)).y(d => y(d[1])).context(context);
+  let values_year_group = _.groupBy(time_series.data, d => d[date_col].getFullYear())
 
-    context.beginPath(), line(values), context.stroke();
+  // For each group add the final value of the next group
 
+  let keys = _.keys(values_year_group)
 
-    const area = d3.area()
-      .x(function(d) {return x(d[2]); })
-      .y0(height)
-      .y1(function(d) { return y(d[1]); })
-      .context(context);
+  let keysmap = _.map(keys, function(d,i) {
+    return [d, keys[i+1]]
+  })
+  keysmap = _.fromPairs(keysmap)
 
-    context.fillStyle = "#DDE"
-
-    values = _.map(values, (d,i) => _.concat(d, [i]))
-
-    let year_groups = _.groupBy(values, d => d[0].slice(0,4))
-
-     year_groups = _.mapValues(year_groups, function(v,k) {
-       let key = `${k}Q1`
-       let start_index = _.findIndex(time_index, d => d == key)
-       let quarters = _.slice(time_index, start_index, start_index+5)
-
-       return _.filter(values, d => _.includes(quarters, d[0]))
-     })
+  _.each(values_year_group, function(values,key) {
+    let nextkey = keysmap[key]
+    if (typeof(nextkey) != 'undefined') {
+      let nextgroup = values_year_group[nextkey]
+      values_year_group[key].push(nextgroup[0])
+     }
+  })
 
 
-    _.each(year_groups, function(c,i) {
+  const x = d3.scaleBand().domain(index).range([0.5, width - 1.5]);
+  const y = d3.scaleLinear().domain([0, _.max(values)]).range([height - 0.5, 0.5]);
+  const context = DOM.context2d(width, height);
 
-      if (i % 2 == 0) {
-        context.fillStyle = "#DDE"
-      } else {
-        context.fillStyle = "#EEF"
-      }
-      context.beginPath(), area(c), context.fill();
+  const line = d3.line()
+                 .x(d => x(d[index_col]))
+                 .y(d => y(d[value_col]))
+                 .context(context);
 
-      // Add year label
-      context.fillStyle = "#444"
-      context.textAlign = "middle";
-      context.textBaseline = "bottom";
-      context.font = "6px sans-serif";
-      let first_in_chunk = c[0]
-      let year = "'" + first_in_chunk[0].slice(2,4)
+  context.beginPath(), line(time_series.data), context.stroke();
 
-      context.fillText(year, x(c[0][2]+1), height*0.8);
+  const area = d3.area()
+    .x(function(d) {return  x(d[index_col])})
+    .y0(height)
+    .y1(function(d) { return y(d[value_col]); })
+    .context(context);
 
+  context.fillStyle = "#DDE"
 
-    })
-    return context.canvas;
-  }
+  _.each(values_year_group, function(c,i) {
+
+    if (i % 2 == 0) {
+      context.fillStyle = "#DDE"
+    } else {
+      context.fillStyle = "#EEF"
+    }
+    context.beginPath(), area(c), context.fill();
+
+    // Add year label
+    context.fillStyle = "#444"
+    context.textAlign = "middle";
+    context.textBaseline = "bottom";
+    context.font = "6px sans-serif";
+    let first_in_chunk = c[0]
+    let last_in_chunk = c.slice(-1)[0]
+
+    let width_of_chunk = x(last_in_chunk[index_col]) - x(first_in_chunk[index_col])
+
+    let year = "'" + (first_in_chunk[date_col].getFullYear() + "").slice(2,4)
+
+    context.fillText(year, x(first_in_chunk[index_col]) + width_of_chunk/2 - 5, height*0.8);
+
+  })
+  return context.canvas;
+}
