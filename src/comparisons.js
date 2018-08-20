2154922252244d0957351e5ee575adf5814ea8c0
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2018-08-20 10:37:33 +0100

    auto add

--- src/comparisons.js
+++ src/comparisons.js
@@ -4,35 +4,3 @@ export let per_fmt = d3.format(",.1%")
 export let int_fmt = d3.format(",.0f")
 export let date_fmt = d3.timeFormat("%d %b %Y")
 
-
-export function percentage_change(values_comparison, value_key) {
-
-    let base = values_comparison['base'][value_key]
-    let comp = values_comparison['comparator'][value_key]
-
-    if (base > comp) {
-      return  per_fmt((base)/comp)
-    } else if (base < comp) {
-      return per_fmt(1-base/comp)
-    } else {
-      return per_fmt(0)
-    }
-
- }
-
-
- export function increase_decrease(values_comparison, value_key) {
-
-  let base = values_comparison['base'][value_key]
-  let comp = values_comparison['comparator'][value_key]
-
-  if (base > comp) {
-   return 'an increase of'
-  } else if (base < comp) {
-    return 'a decrease of'
-  } else {
-    return 'a change of'
-  }
-}
-
-
